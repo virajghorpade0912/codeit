8ed02106250813dc78aacf4da3ddb686da8e0e1e
viraj <viraj.ghorpade9767@gmail.com>
2020-04-04 20:09:16 +0530

    Updates

---
+++ precache-manifest.776dc0a7045ddd46c711df6958943a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac101db4248f724576fef73f252ef3e",
+    "url": "/virajghorpade0912/codeit.git/index.html"
+  },
+  {
+    "revision": "7c18f3d4ddddfa0bf70d",
+    "url": "/virajghorpade0912/codeit.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ed8102f8d1c0acab776",
+    "url": "/virajghorpade0912/codeit.git/static/js/2.7aa3d51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/virajghorpade0912/codeit.git/static/js/2.7aa3d51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c18f3d4ddddfa0bf70d",
+    "url": "/virajghorpade0912/codeit.git/static/js/main.530b610a.chunk.js"
+  },
+  {
+    "revision": "305bc411c1e79c95ba9c",
+    "url": "/virajghorpade0912/codeit.git/static/js/runtime-main.2e76dee4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/virajghorpade0912/codeit.git/precache-manifest.776dc0a7045ddd46c711df6958943a7c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/virajghorpade0912/codeit.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
